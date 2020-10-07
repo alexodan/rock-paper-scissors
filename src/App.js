26368ad61085f4c9a2d77d3c56f04b2878900e58
alexodan <alexdan1993@gmail.com>
2020-10-06 23:59:17 -0300

    Removes comments and console logs.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import VersusBoard from "./components/Versus";
 import Header from "./components/Header";
 
 export default function App() {
-  const [showIntro, setShowingIntro] = useState(false); // TODO: useLocalStorage
-  const [score, setScore] = useState(0); // TODO: useLocalStorage
+  const [showIntro, setShowingIntro] = useState(false);
+  const [score, setScore] = useState(0);
   const [userOption, setUserOption] = useState("");
 
   const updateScore = (matchResult, points) => {
-    console.log("Updating score:", matchResult, points);
     if (matchResult === "win") {
       setScore(score + points);
-    } else if (matchResult === "loose") {
+    } else if (matchResult === "lose") {
       setScore(score - points < 0 ? score : score - points);
     }
   };
--- src/hooks/useLocalStorage.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const useLocalStorage = (label) => {
-  const [storage, setStorage] = useState(label);
-
-  return [storage, setStorage];
-};
-
-export default useLocalStorage;
